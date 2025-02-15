c1046da990720f1cb3a23620753eae43f7158792
lucielcastro <lucielcastro.dev@gmail.com>
2025-02-15 20:02:26 -0300

    add create product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product; // ✅ Importação do modelo Product
+use HasFactory;
 
 class ProductController extends Controller
 {
     public function index(){
         return view('product.index');
+    }
+    
+    public function create(){
+        return view('product.create');
+    }
+
+    public function store(Request $request){
+        // Validação dos dados do formulário
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/', 'description'=>'required'
+        ]);
+
+        // Criar um novo produto no banco de dados
+        $newProduct = Product::create($data);
+
+        // Redirecionar para a listagem de produtos
+        return redirect(route('product.create'));
     }
 }
--- database/migrations/2025_02_15_210034_create_products_table.php
+++ database/migrations/2025_02_15_210034_create_products_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('produts', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->integer('qty');
